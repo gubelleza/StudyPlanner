e730399dd4f3b7fe1aaa273ce1d1131d27452aa7
Gustavo Belleza <dias.gcb@gmail.com>
2020-08-22 17:52:29 -0300

    adicionados comportamentos usando js

--- AgendaEstudos/wwwroot/js/tarefas.js
+++ AgendaEstudos/wwwroot/js/tarefas.js
@@ -1,8 +1,58 @@
+// Function definitions
+// range input functions
 
-function outputPrioridade() {
-    document.getElementById("#out-prioridade").innerText =
-        document.getElementById("#prioridade").getAttribute("value");
+class RangeOutput {
+    
+    textStyle;
+    element;
+    
+    constructor(textStyle) {
+        this.textStyle = textStyle;
+        this.element = document.getElementById("outPrioridade")
+    }
+    
+    setText(text) {
+        this.element.innerHTML = text;
+    }
+    
+    setStyle(newStyle) {
+        this.element.classList.replace(this.textStyle, newStyle);
+        this.textStyle = newStyle;
+    }
+    
+    applyPriorityStyle = (input) => {
+        switch (input.value) {
+            case "1":
+                this.setStyle("text-success")
+                break;
+            case "1.5":
+                this.setStyle("text-info");
+                break;
+            case "2":
+                this.setStyle("text-dark");
+                break;
+            case "2.5":
+                this.setStyle("text-warning");
+                break;
+            case "3":
+                this.setStyle("text-danger");
+                break;
+            default:
+                this.setStyle("text-dark");
+                console.log("default")
+                break;
+        }
+    }
 }
 
-document.getElementById("#prioridade")
-    .addEventListener("mouseup", outputPrioridade);
+// Function calls
+// Range input function calls
+let input = document.getElementById("inputPrioridade");
+let output = new RangeOutput("text-dark")
+
+output.setText(input.value);
+
+input.oninput = function (){    
+    output.element.innerHTML = this.value;
+    output.applyPriorityStyle(this)
+}
